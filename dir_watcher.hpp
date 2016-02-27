416070e56267bd55a62416c2c31abab3cc4c8a7e
emilk <emil.ernerfeldt@gmail.com>
2016-02-27 15:22:52 +0100

    Add dir_watcher: watch for files getting modified/added/deleted

---
+++ dir_watcher.hpp
@@ -0,0 +1,84 @@
+/*
+Copyright 2016 Emil Ernerfeldt
+Created 2014-2015 for Ghostel
+Adapted 2016 for revived PipeDreams
+*/
+
+#pragma once
+
+#include <ctime>
+#include <set>
+#include <string>
+#include <vector>
+
+struct kevent;
+
+namespace emilib {
+
+// Watches for any changes in a directory (file changed, added, removed).
+class DirWatcher
+{
+public:
+	// Feel free to end with a slash or not.
+	explicit DirWatcher(std::string dir);
+	~DirWatcher();
+
+	// Returns a list of paths to files that where added, removed or changed.
+	std::vector<std::string> poll_files();
+
+private:
+	DirWatcher(DirWatcher&) = delete;
+	DirWatcher(DirWatcher&&) = delete;
+	DirWatcher& operator=(DirWatcher&) = delete;
+	DirWatcher& operator=(DirWatcher&&) = delete;
+
+	struct File
+	{
+		std::string       path;
+		std::string       file_name;
+		time_t            mtime;
+		bool              is_dir;
+		std::vector<File> children;
+		int               fd = -1; // File descriptor
+	};
+
+	void create();
+	void add_dir(std::string dir);
+	void destroy();
+
+	void add_dir(File& dir);
+	void poll_files_in(std::vector<std::string>& changes, File& dir);
+	void rescan(std::vector<std::string>& changes, File& dir);
+
+	void add_kevent(File& file);
+
+	void close_file(File& file);
+
+private:
+	const std::string   _dir;
+	bool                _recursive   = true;
+	bool                _check_files = true;
+	std::vector<File>   _dirs;
+	std::vector<kevent> _events;
+	int                 _kqueue;
+};
+
+// -------------------------------------------------------
+
+// Acts like DirWatcher but with a delay of a few frames to let things 'settle'
+class DelayedDirWatcher
+{
+public:
+	// frame_delay: wait this many calls to poll_files before reporting a change.
+	DelayedDirWatcher(std::string dir, unsigned frame_delay = 6);
+
+	std::vector<std::string> poll_files();
+
+private:
+	const unsigned        _frame_delay;
+	DirWatcher            _dir_watcher;
+	std::set<std::string> _dirty_files;
+	unsigned              _frames_since_last_change = 0;
+};
+
+} // namespace emilib
