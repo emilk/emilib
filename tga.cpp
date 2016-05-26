93308b58ac8cc69db05093ceb14032d7a4fa6335
emilk <emil.ernerfeldt@gmail.com>
2016-05-26 21:40:38 +0200

    optimize tga writing

--- tga.cpp
+++ tga.cpp
@@ -5,59 +5,75 @@
 
 namespace emilib {
 
-bool write_tga(const char* path, size_t width, size_t height, const void* rgba_ptr, bool include_alpha)
+std::vector<uint8_t> encode_tga(size_t width, size_t height, const void* rgba_ptr, bool include_alpha)
 {
 	CHECK_LE_F(width,  0xFFFF);
 	CHECK_LE_F(height, 0xFFFF);
 
-	FILE* fp = fopen(path, "w");
-	if (fp == nullptr) {
-		LOG_F(ERROR, "Failed to open '%s' for writing", path);
-		return false;
-	}
+	const size_t TGA_HEADER_SIZE = 18;
+	const size_t TGA_FOOTER_SIZE = 26;
+	const size_t data_size       = width * height * (include_alpha ? 4 : 3);
 
-	// The image header
-	char header[ 18 ] = { 0 }; // char = byte
-	header[ 2 ] = 2; // truecolor
-	header[ 12 ] = width & 0xFF;
-	header[ 13 ] = (width >> 8) & 0xFF;
-	header[ 14 ] = height & 0xFF;
-	header[ 15 ] = (height >> 8) & 0xFF;
-	header[ 16 ] = include_alpha ? 32 : 24; // bits per pixel
+	std::vector<uint8_t> data(TGA_HEADER_SIZE + data_size + TGA_FOOTER_SIZE, 0);
 
-	fwrite((const char*)&header, 1, sizeof(header), fp);
+	// Write header:
+	data[  2 ] = 2; // truecolor
+	data[ 12 ] = width & 0xFF;
+	data[ 13 ] = (width >> 8) & 0xFF;
+	data[ 14 ] = height & 0xFF;
+	data[ 15 ] = (height >> 8) & 0xFF;
+	data[ 16 ] = include_alpha ? 32 : 24; // bits per pixel
+
+	size_t index = TGA_HEADER_SIZE;
 
 	// The image data is stored bottom-to-top, left-to-right
 	for (int y = (int)height - 1; y >= 0; y--) {
 		for (int x = 0; x < width; x++) {
 			uint32_t rgba = *((const uint32_t*)rgba_ptr + y*width + x);
 			if (include_alpha) {
-				char r = (rgba & 0x000000FF);
-				char g = (rgba & 0x0000FF00) >> 8;
-				char b = (rgba & 0x00FF0000) >> 16;
-				char a = (rgba & 0xFF000000) >> 24;
-				putc((int)(b & 0xFF), fp);
-				putc((int)(g & 0xFF), fp);
-				putc((int)(r & 0xFF), fp);
-				putc((int)(a & 0xFF), fp);
+				uint8_t r = (rgba & 0x000000FF);
+				uint8_t g = (rgba & 0x0000FF00) >> 8;
+				uint8_t b = (rgba & 0x00FF0000) >> 16;
+				uint8_t a = (rgba & 0xFF000000) >> 24;
+				data[index++] = b & 0xFF;
+				data[index++] = g & 0xFF;
+				data[index++] = r & 0xFF;
+				data[index++] = a & 0xFF;
 			} else {
-				char r = (rgba & 0x0000FF);
-				char g = (rgba & 0x00FF00) >> 8;
-				char b = (rgba & 0xFF0000) >> 16;
-				putc((int)(b & 0xFF), fp);
-				putc((int)(g & 0xFF), fp);
-				putc((int)(r & 0xFF), fp);
+				uint8_t r = (rgba & 0x0000FF);
+				uint8_t g = (rgba & 0x00FF00) >> 8;
+				uint8_t b = (rgba & 0xFF0000) >> 16;
+				data[index++] = b & 0xFF;
+				data[index++] = g & 0xFF;
+				data[index++] = r & 0xFF;
 			}
 		}
 	}
 
-	// The file footer
-	static const char footer[ 26 ] =
-		"\0\0\0\0" // no extension area
-		"\0\0\0\0" // no developer directory
-		"TRUEVISION-XFILE" // yep, this is a TGA file
-		".";
-	fwrite((const char*)&footer, 1, sizeof(footer), fp);
+	CHECK_EQ_F(index, TGA_HEADER_SIZE + data_size);
+
+	memcpy(&data[index], "\0\0\0\0\0\0\0\0", 8); // no extension area + no developer directory
+	index += 8;
+
+	memcpy(&data[index], "TRUEVISION-XFILE.", strlen("TRUEVISION-XFILE." + 1));
+	index += strlen("TRUEVISION-XFILE.") + 1;
+
+	CHECK_EQ_F(index, data.size());
+
+	return data;
+}
+
+bool write_tga(const char* path, size_t width, size_t height, const void* rgba_ptr, bool include_alpha)
+{
+	FILE* fp = fopen(path, "w");
+	if (fp == nullptr) {
+		LOG_F(ERROR, "Failed to open '%s' for writing", path);
+		return false;
+	}
+
+	const auto data = encode_tga(width, height, rgba_ptr, include_alpha);
+
+	fwrite(data.data(), 1, data.size(), fp);
 
 	fclose(fp);
 	return true;
--- tga.hpp
+++ tga.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
 namespace emilib {
 
 // Dump image to disk as .tga. Image should be stored row by row, top-left to bottom-right.
 // rgba_ptr should point to width * height * 4 bytes.
-// If include_alpha is false, the alpha channel will be ommited.
+// If include_alpha is false, the alpha channel will be omitted.
 bool write_tga(const char* path, size_t width, size_t height, const void* rgba_ptr, bool include_alpha);
+
+/// As above, but returns the tga in memory.
+std::vector<uint8_t> encode_tga(size_t width, size_t height, const void* rgba_ptr, bool include_alpha);
 
 } // namespace emilib
