cb0cfd60fa0ed30f4b1533509434a575b3ce46b4
emilk <emil.ernerfeldt@gmail.com>
2016-08-28 16:39:37 +0200

    Minor fix

--- emilib/music.mm
+++ emilib/music.mm
@@ -3,6 +3,7 @@
 //   This software is dual-licensed to the public domain and under the following
 //   license: you are granted a perpetual, irrevocable license to copy, modify,
 //   publish, and distribute this file as you see fit.
+#include "music.hpp"
 
 #include <string>
 
