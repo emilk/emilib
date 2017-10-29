69ea4e67c51c0b36bf1a5f092ac7ab2d49185dd7
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2017-10-29 12:06:19 +0100

    expand algorithm.hpp with some helpful functions

--- emilib/algorithm.hpp
+++ emilib/algorithm.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <algorithm>
+#include <numeric>
 
 #include <loguru.hpp>
 
@@ -30,4 +31,48 @@ void erase_if(Vector* vec, const Predicate& predicate)
 	vec->erase(it, std::end(*vec));
 }
 
+/// Apply a function to each element and return the results.
+template <typename Container, typename Kernel>
+auto map(Container&& inputs, const Kernel& kernel)
+    -> std::vector<decltype(kernel(inputs.front()))>
+{
+	std::vector<decltype(kernel(inputs.front()))> results;
+	results.reserve(inputs.size());
+	for (auto&& input_value : inputs) {
+		results.emplace_back(kernel(input_value));
+	}
+	return results;
 }
+
+/// Sort the given range so elements with lower key(element) comes first.
+template<typename Vector, typename Key>
+void stable_sort_by_key(Vector* vec, const Key& key)
+{
+	CHECK_NOTNULL_F(vec);
+	std::stable_sort(std::begin(*vec), std::end(*vec), [&key](const auto& a, const auto& b)
+	{
+		return key(a) < key(b);
+	});
+}
+
+template<typename Vector>
+bool all_same(const Vector& v)
+{
+	CHECK_F(!v.empty());
+	for (int i=1; i<v.size(); ++i) {
+		if (v[i] != v[0]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+template<typename Vector>
+int sum(const Vector& v)
+{
+	using namespace std;
+	using value_type = typename Vector::value_type;
+	return std::accumulate(begin(v), end(v), value_type());
+}
+
+} // namespace emilib
