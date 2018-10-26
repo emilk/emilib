78f42ba459c98e75b2883dbbe5a74f46b17573b7
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2018-10-26 18:32:44 +0200

    Implement operators on ImVec2 and ImVec4

--- emilib/imgui_helpers.hpp
+++ emilib/imgui_helpers.hpp
@@ -11,25 +11,25 @@
 #include <string>
 #include <vector>
 
-#define IM_VEC2_CLASS_EXTRA                                                                        \
-    friend ImVec2 operator+(const ImVec2& a, const ImVec2& b) { return {a.x + b.x, a.y + b.y}; }   \
-    friend ImVec2 operator-(const ImVec2& a, const ImVec2& b) { return {a.x - b.x, a.y - b.y}; }   \
-    friend ImVec2 operator*(const ImVec2& v, float s) { return {v.x * s, v.y * s}; }               \
-    friend ImVec2 operator*(float s, const ImVec2& v) { return {v.x * s, v.y * s}; }               \
-    friend bool operator==(const ImVec2& a, const ImVec2& b) { return a.x == b.x && a.y == b.y; }  \
-    void operator*=(float s) { x *= s; y *= s; }                                                   \
-    void operator/=(float s) { x /= s; y /= s; }                                                   \
-    float norm() { return std::hypot(x, y); }
-
-#define IM_VEC4_CLASS_EXTRA                                                        \
-    friend bool operator==(const ImVec4& a, const ImVec4& b) {                     \
-        return a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w; }             \
-
 #include <imgui/imgui.h>
 
 using GLuint = uint32_t;
 
 namespace imgui_helpers {
+
+static inline ImVec2 operator*(const ImVec2& lhs, const float rhs)              { return ImVec2(lhs.x*rhs, lhs.y*rhs); }
+static inline ImVec2 operator/(const ImVec2& lhs, const float rhs)              { return ImVec2(lhs.x/rhs, lhs.y/rhs); }
+static inline ImVec2 operator+(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x+rhs.x, lhs.y+rhs.y); }
+static inline ImVec2 operator-(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x-rhs.x, lhs.y-rhs.y); }
+static inline ImVec2 operator*(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x*rhs.x, lhs.y*rhs.y); }
+static inline ImVec2 operator/(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x/rhs.x, lhs.y/rhs.y); }
+static inline ImVec2& operator+=(ImVec2& lhs, const ImVec2& rhs)                { lhs.x += rhs.x; lhs.y += rhs.y; return lhs; }
+static inline ImVec2& operator-=(ImVec2& lhs, const ImVec2& rhs)                { lhs.x -= rhs.x; lhs.y -= rhs.y; return lhs; }
+static inline ImVec2& operator*=(ImVec2& lhs, const float rhs)                  { lhs.x *= rhs; lhs.y *= rhs; return lhs; }
+static inline ImVec2& operator/=(ImVec2& lhs, const float rhs)                  { lhs.x /= rhs; lhs.y /= rhs; return lhs; }
+static inline ImVec4 operator+(const ImVec4& lhs, const ImVec4& rhs)            { return ImVec4(lhs.x+rhs.x, lhs.y+rhs.y, lhs.z+rhs.z, lhs.w+rhs.w); }
+static inline ImVec4 operator-(const ImVec4& lhs, const ImVec4& rhs)            { return ImVec4(lhs.x-rhs.x, lhs.y-rhs.y, lhs.z-rhs.z, lhs.w-rhs.w); }
+static inline ImVec4 operator*(const ImVec4& lhs, const ImVec4& rhs)            { return ImVec4(lhs.x*rhs.x, lhs.y*rhs.y, lhs.z*rhs.z, lhs.w*rhs.w); }
 
 /* Examples:
     if (ImGui::BeginMainMenuBar()) {
